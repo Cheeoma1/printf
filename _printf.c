cdf8d1bbf9e8e8e922e3916d29c5736654352725
Hamdi-larbi <larbi.hamdi@gmail.com>
2023-01-25 14:15:29 -0800

    update

--- _printf.c
+++ _printf.c
@@ -68,8 +68,8 @@ int _printf(const char *format, ...)
 				free(dig);
 			}
 		}
-		else if (format[i] == '\n')
-			count += write(1, &format[i], 1) - 1;
+/*		else if (format[i] == '\n')
+			count += write(1, &format[i], 1) - 1;*/
 		else
 			count += write(1, &format[i], 1);
 	}
